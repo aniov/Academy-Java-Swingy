71c57d91e4c69726a0f94ffb7fc771131346822b
Marius Voina <aniov_usa@yahoo.com>
2017-06-18 17:20:45 +0300

    data base connection

--- src/main/java/aniov/company/App.java
+++ src/main/java/aniov/company/App.java
@@ -1,10 +1,16 @@
 package aniov.company;
 
+import aniov.company.service.database.DataBaseUtil;
+
 /**
  * RPG Game Entry point
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        DataBaseUtil dataBaseUtil = new DataBaseUtil();
+        dataBaseUtil.connectToDb();
+
     }
 }
