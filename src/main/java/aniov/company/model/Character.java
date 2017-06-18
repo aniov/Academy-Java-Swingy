dd5121f62620bd8da318024d1568dfc839627b16
Marius Voina <aniov_usa@yahoo.com>
2017-06-18 16:45:33 +0300

    Initial commit

---
+++ src/main/java/aniov/company/model/Character.java
@@ -0,0 +1,16 @@
+package aniov.company.model;
+
+import lombok.Data;
+
+/**
+ * Created by aniov on 6/18/2017.
+ */
+@Data
+public class Character {
+
+    private Integer level;
+    private Integer experience;
+    private Integer attack;
+    private Integer defence;
+    private Integer hitPoints;
+}
